119a37a8651c11284e91d71eb8615ec394f3ea58
Jonathan Zapotosczny <JonathanGM.HD@gmail.com>
2024-01-13 18:38:18 -0300

    feature: exceções personalidazadas

---
+++ src/main/java/br/com/gerenciamentosimples/exception/DataIntegrityViolationException.java
@@ -0,0 +1,12 @@
+package br.com.gerenciamentosimples.exception;
+
+public class DataIntegrityViolationException extends RuntimeException {
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
